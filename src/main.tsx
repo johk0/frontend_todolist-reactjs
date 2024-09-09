17040aa63eaf02ed63e0d333e377c49c9c6aafeb
Youssef Elaraby <y.ahmed.ea@gmail.com>
2024-09-09 12:24:19 +0300

    enhancements

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,16 @@ import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
+	defaultOptions: {
+		queries: {
+			// deisable refetch to get data if updated on window focus
+			// refetchOnWindowFocus: false,
+		},
+	},
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
+	<QueryClientProvider client={queryClient}>
+		<App />
+	</QueryClientProvider>
 );
